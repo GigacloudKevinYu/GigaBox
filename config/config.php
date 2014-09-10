ad00106e27fd5b82f1a44f47526f7d02d144da4a
GigacloudKevinYu <kevin.yu@gigacloud.com.tw>
2014-09-10 10:22:50 +0800

    System Initialization

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc5cf95b9cd2',
+  'passwordsalt' => '5bdb3ae258a0ab26e12bf704f48884',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+  ),
+  'datadirectory' => 'D:\\GigaBox\\data',
+  'dbtype' => 'mysql',
+  'version' => '7.0.1.1',
+  'dbname' => 'GigaBox',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'GigaBox',
+  'dbpassword' => 'P@ssw0rd',
+  'installed' => true,
+);
