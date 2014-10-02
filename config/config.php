17076e552397229fdfc6458c61cb8cfafd67c91c
Hsinyaoyu <keivnyu@gigacloud.com.tw>
2014-10-02 14:53:29 +0800

    commit config.php

--- config/config.php
+++ config/config.php
@@ -2,10 +2,10 @@
 $CONFIG = array (
   'instanceid' => 'ocylhij7d10o',
   'passwordsalt' => '3ppr04ra7bkm374tsd67o8hzij9ajz',
-  'trusted_domains' => 
-  array (
-    0 => 'localhost',
-  ),
+//  'trusted_domains' =>	// Disable for no access constraint
+//  array (
+//    0 => 'localhost',
+//  ),
   'datadirectory' => 'C:\\wamp\\www\\GigaBox\\data',
   'dbtype' => 'mysql',
   'version' => '7.0.1.1',
@@ -15,4 +15,6 @@ $CONFIG = array (
   'dbuser' => 'GigaBox',
   'dbpassword' => 'P@ssw0rd',
   'installed' => true,
+  'updatechecker' => false	// Check if ownCloud is up to date		
 );
+DEFINE('DEBUG',true)	// Debug mode (no cache)
